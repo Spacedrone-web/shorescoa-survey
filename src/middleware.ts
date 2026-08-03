dbf2622bce5a684424d950b156cdabd206a1067b
Spacedrone-web <jim@fleming2.com>
2026-08-03 08:32:38 -0600

    Fix middleware + make date and unit fields optional

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-﻿import { defineMiddleware } from 'astro:middleware';
+import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const { pathname } = context.url;
@@ -13,23 +13,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
     const urlToken    = context.url.searchParams.get('token');
     const cookieToken = context.cookies.get('survey_auth')?.value;
-
-
-
-$file = "D:\shorescoa-survey\src\pages\index.astro"
-$content = Get-Content $file -Raw
-
-# Remove required from arrival date, departure date, and unit number inputs
-$content = $content -replace '(<input[^>]*id="arrival_date"[^>]*)\s*required([^>]*>)', '$1$2'
-$content = $content -replace '(<input[^>]*id="departure_date"[^>]*)\s*required([^>]*>)', '$1$2'
-$content = $content -replace '(<input[^>]*id="unit_number"[^>]*)\s*required([^>]*>)', '$1$2'
-
-Set-Content $file -Value $content -Encoding UTF8
-
-git -C D:\shorescoa-survey add src/pages/index.astro
-git -C D:\shorescoa-survey commit -m "Make arrival date, departure date, and unit number optional"
-git -C D:\shorescoa-survey push
-
 
     if (urlToken === SURVEY_TOKEN) {
       context.cookies.set('survey_auth', SURVEY_TOKEN, {
